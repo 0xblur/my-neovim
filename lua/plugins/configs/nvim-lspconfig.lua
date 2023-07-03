ea2d3aa15b91391c1bb0e07ff2c02f50a9dfb205
cypherhades <112914234+0xblur@users.noreply.github.com>
2023-07-03 17:36:07 -0500

    Added LSP
    I can't make Mason to automatically install LSP yet

---
+++ lua/plugins/configs/nvim-lspconfig.lua
@@ -0,0 +1,4 @@
+return function(_, opts)
+  require"nvim-lspconfig".setup(opts)
+end
+
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,8 +1,58 @@
 return {
-	-- Install LSP and Mason LSP manager
-	{
-		"neovim/nvim-lspconfig",
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim"
-	}
+  -- Install Mason
+  {
+    "williamboman/mason.nvim",
+    cmd = {
+      "Mason",
+      "MasonInstall",
+      "MasonUninstall",
+      "MasonUninstallAll",
+      "MasonLog",
+      "MasonUpdate",
+      "MasonUpdateAll",
+    },
+    opts = {
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_uninstalled = "✗",
+          package_pending = "⟳",
+        },
+      },
+      pip = {
+        upgrade_pip = true,
+      }
+    },
+    build = ":MasonUpdate",
+    config = require "plugins.configs.mason",
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    cmd = {
+      "LspInstall",
+      "LspUninstall",
+    },
+    opts = {
+      ensure_installed = { "lua_ls", "marksman", "tsserver", "ruff_lsp", "tsserver" },
+      automatic_installation = true
+    },
+    config = require "plugins.configs.mason-lspconfig"
+  },
+  {
+    "neovim/nvim-lspconfig",
+    cmd = {
+      "LspStart",
+      "LspInfo"
+    },
+    opts = function()
+      local lspconfig = require'lspconfig'
+      lspconfig.ruff_lsp.setup {}
+      lspconfig.tsserver.setup {}
+      lspconfig.rust_analyzer.setup {}
+      lspconfig.lua_ls.setup {}
+      lspconfig.marksman.setup {}
+    end,
+    config = true
+  }
 }
