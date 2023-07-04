00971186e7445a5778dfaa4de7edc1a594b69de3
cypherhades <112914234+0xblur@users.noreply.github.com>
2023-07-04 07:34:33 -0500

    Added filetypes to LSP servers

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -37,7 +37,7 @@ return {
       ensure_installed = { "lua_ls", "marksman", "tsserver", "ruff_lsp", "tsserver" },
       automatic_installation = true
     },
-    config = require "plugins.configs.mason-lspconfig"
+    config = require"plugins.configs.mason-lspconfig"
   },
   {
     "neovim/nvim-lspconfig",
@@ -47,12 +47,22 @@ return {
     },
     opts = function()
       local lspconfig = require'lspconfig'
-      lspconfig.ruff_lsp.setup {}
-      lspconfig.tsserver.setup {}
-      lspconfig.rust_analyzer.setup {}
-      lspconfig.lua_ls.setup {}
-      lspconfig.marksman.setup {}
+      lspconfig.ruff_lsp.setup {
+        filetypes = { "py", "ipynb" }
+      }
+      lspconfig.tsserver.setup {
+        filetypes = { "rs" }
+      }
+      lspconfig.rust_analyzer.setup {
+        filetypes = { "rs" }
+      }
+      lspconfig.lua_ls.setup {
+        filetypes = { "lua" }
+      }
+      lspconfig.marksman.setup {
+        filetypes = { "md" }
+      }
     end,
-    config = true
+    config = require"plugins.configs.nvim-lspconfig"
   }
 }
